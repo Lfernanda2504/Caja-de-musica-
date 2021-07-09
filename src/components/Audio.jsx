b8450c37f60d0f7daa3faa2228ce5e01c2efadd7
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-07-08 23:16:56 -0500

    agregando evento a el boton inicio

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,21 +1,81 @@
-import React from 'react';
-import  Pad  from '../components/Pad';
-import {audio} from '../data/audios';
+import React, { useState } from "react";
+import Pad from "../components/Pad";
+import { audio } from "../data/audios";
 
-
-  function Audio() {
-    console.log({audio});
-    return (
-      <div className="bg-light min-vh-100 text-black">
-        <h2 className="text-center">
-          Drum Machine 
-        </h2>
-        {audio.map((clip) =>(
-          <Pad key={clip.id} clip={clip}/>
-        ))}
-        
-      </div>
-    )
-  }
+function Audio() {
+  const [volumen, setVolumen] = useState(1);
+  const [recording, setRecording] = useState("");
+  const [speed, setSpeed] = useState(0.5);
+  //verificando array
+  console.log({ audio });
   
-  export default Audio;
+  const playRecording = () => {
+    let index = 0;
+    let recordArray = recording.split("");
+    const interval = setInterval(() => {
+      const audioTag = document.getElementById(recordArray[index]);
+      audioTag.volumen = volumen;
+      audioTag.currentTime = 0;
+      audioTag.play();
+      index++;
+    },speed * 600);
+    setTimeout(()=> clearInterval(interval),
+       600 * speed * recordArray.length-1);
+  };
+
+  return (
+    <div className="bg-light min-vh-100 text-black">
+      <div className="text-center">
+        <h2>Drum Machine</h2>
+        {audio.map((clip) => (
+          <Pad
+            key={clip.id}
+            clip={clip}
+            volumen={volumen}
+            setRecording={setRecording}
+          />
+        ))}
+
+        <h4>Volumen</h4>
+        <input
+          type="range"
+          step="0.001"
+          onChange={(e) => setVolumen(e.target.value)}
+          value={volumen}
+          max="1"
+          min="0"
+          className="w-50 progress-bar-striped bg-info"
+        />
+        <h3>{recording}</h3>
+        {recording && (
+          <>
+            <button
+              onClick={playRecording}
+              className="btn btn btn-outline-info mx-4"
+            >
+              Iniciar
+            </button>
+            <button
+              onClick={() => setRecording("")}
+              className="btn btn-outline-secondary mx-4">
+              Limpiar
+            </button>
+          </>
+          
+        )}
+        <h4>Speed</h4>
+        <input
+          type="range"
+          step="0.001"
+          onChange={(e) => setSpeed(e.target.value)}
+          value={speed}
+          max="1.2"
+          min="0.1"
+          className="w-50"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Audio;
--- src/components/Pad.jsx
+++ src/components/Pad.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 
-function Pad({clip}) {
-   const playSound = () => {
-    const audioTag = document.getElementById(clip.keyL);
-    audioTag.currentTime= 0;
-    audioTag.play(); 
+function Pad({clip, volumen, setRecording}) {
+    const [active, setActive] = useState(false);
+   useEffect(() => {
+       document.addEventListener('keydown', handleKeyPress);
+       return () => {
+        document.removeEventListener('keydown', handleKeyPress);   
+       }
+   }, []);
+
+   const handleKeyPress = (e) =>{
+        if(e.keyCode === clip.keyCode){
+            playSound();
+        }
+   };
+   
+    const playSound = () => {
+        const audioTag = document.getElementById(clip.keyL);
+        setActive(true);
+        setTimeout(()=> setActive(false), 200);
+        audioTag.volumen= volumen;
+        audioTag.currentTime= 0;
+        audioTag.play();
+        setRecording(prev => prev+clip.keyL + ""); 
     };
     
     return (
-        <div onClick={playSound} className="btn btn-info p-4 m-3">
+        <div onClick={playSound} className={`drum-pad btn btn-info p-4 m-3 ${active && "drum-pad btn-primary"}`}>
             <audio className="clip" id={clip.keyL} src={clip.url}/>
             {clip.keyL}
         </div>
-    )
+    );
 }
 
-export default Pad
+export default Pad;
 
